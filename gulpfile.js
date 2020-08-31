0ae6db6b06e65b3009509bdd226c54e5019c5245
Betkyss <betko1@mail.ru>
2020-08-31 13:41:17 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,116 @@
+const {
+	src,
+	dest,
+	parallel,
+	series,
+	watch
+} = require('gulp');
+
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+const imgmin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const del = require('del');
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/', // Путь у файлам
+			notify: true, // Увидомления
+			// online: false, // Работает без интернета
+
+		}
+	})
+}
+
+const sctiptsFiles = [
+	'node_modules/jquery/dist/jquery.min.js',
+	'node_modules/bootstrap/dist/js/bootstrap.js',
+	'app/js/src/main.js',
+];
+const stylesFiles = [
+	'node_modules/bootstrap/dist/css/bootstrap.css',
+	'app/css/src/main.scss',
+];
+
+function scripts() {
+	return src(sctiptsFiles)
+		.pipe(concat('script.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js/dest'))
+		.pipe(browserSync.stream())
+}
+
+
+function styles() {
+	return src(stylesFiles)
+		.pipe(sass())
+		.pipe(concat('style.css'))
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['last 10 versions'],
+			grid: true
+		}))
+		.pipe(cleancss(({
+			level: {
+				1: {
+					specialComments: 0
+				}
+			}
+		})))
+		.pipe(dest('app/css/dest'))
+		.pipe(browserSync.stream())
+}
+
+function images() {
+	return src('app/img/src/**/*')
+		.pipe(newer('app/img/dest/'))
+		.pipe(imgmin())
+		.pipe(dest('app/img/dest/'))
+}
+
+function cleandest() {
+	return del([
+		'app/img/dest/**/*',
+		'app/js/dest/**/*',
+		'app/css/dest/**/*'
+	])
+}
+
+function cleandist() {
+	return del('dist/**/*')
+}
+
+function buildcopy() {
+	return src([
+			'app/css/dest/**/*',
+			'app/js/dest/**/*',
+			'app/img/dest/**/*',
+			'app/**/*.html',
+		], {
+			base: 'app'
+		})
+		.pipe(dest('dist'));
+}
+
+function startwatch() {
+	watch('app/css/src/**/*', styles);
+	watch(['app/js/src/**/*'], scripts);
+	watch('app/**/*.html').on('change', browserSync.reload);
+	watch('app/img/src/**/*', images)
+}
+
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.images = images;
+exports.cleandest = cleandest;
+exports.cleandist = cleandist;
+exports.buildcopy = buildcopy;
+// exports.build = series(cleandist, scripts, styles, images, buildcopy);
+
+// exports.default = parallel(scripts, styles, images, browsersync, startwatch);
+exports.default = series(cleandist, scripts, styles, images, buildcopy, parallel(browsersync, startwatch));
